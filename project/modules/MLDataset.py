1aef0fdeb1bf7715a741937a37bad45956f4791d
reona.investing <reona.investing@gmail.com>
2024-10-31 12:45:42 +0900

    MLDatasetの不具合修正

--- project/modules/MLDataset.py
+++ project/modules/MLDataset.py
@@ -75,6 +75,19 @@ class MLDataset:
         features_df = features_df.set_index(['Date', 'Sector'], drop=True)
         return target_df, features_df
 
+    def _append_next_business_day_row_to_target(self, target_df:pd.DataFrame) -> pd.DataFrame:
+        '''target, featuresの各データフレームに、次の営業日の行を追加'''
+        next_open_date = fetcher.get_next_open_date(latest_date=target_df.index.get_level_values('Date')[-1])
+        sectors = target_df.index.get_level_values('Sector').unique()
+        new_rows = [[next_open_date for _ in range(len(sectors))],[sector for sector in sectors]]
+        target_to_add = pd.DataFrame(index=new_rows, columns=target_df.columns)
+        target_to_add.index.names = ['Date', 'Sector']
+        target_to_add = target_to_add.dropna(axis=1, how='all') # すべてがNAの列を削除
+        target_df = pd.concat([target_df, target_to_add], axis=0).reset_index(drop=False)
+        target_df['Date'] = pd.to_datetime(target_df['Date'])
+        target_df = target_df.set_index(['Date', 'Sector'], drop=True)
+        return target_df
+
     def _split_train_test(self, df:pd.DataFrame, train_start_day:datetime, train_end_day:datetime, test_start_day:datetime, test_end_day:datetime) -> Tuple[pd.DataFrame, pd.DataFrame]:
         '''訓練データとテストデータに分ける'''
         train_df = df[(df.index.get_level_values('Date')>=train_start_day)&(df.index.get_level_values('Date')<=train_end_day)]
@@ -94,15 +107,18 @@ class MLDataset:
                     test_start_day:datetime, test_end_day:datetime,
                     raw_target_df: pd.DataFrame, order_price_df: pd.DataFrame,
                     outlier_theshold:float = 0, no_shift_features:list = [], 
-                    add_next_business_day:bool = True):
+                    reuse_features_df_of_others:bool = False):
         '''
         目的変数と特徴量を格納 :param float outlier_theshold: 外れ値除去の閾値（±何σ）？
         '''
         # 次の営業日の行を追加して、featuresの行を1日ぶん後ろにずらす
-        if add_next_business_day:
+        if reuse_features_df_of_others:
+            target_df = self._append_next_business_day_row_to_target(target_df)
+        else:
             target_df, features_df = self._append_next_business_day_row(target_df, features_df)
+            
         shift_features = [col for col in features_df.columns if col not in no_shift_features]
-        features_df[shift_features] = features_df.groupby('Sector')[shift_features].shift(1) 
+        features_df[shift_features] = features_df.groupby('Sector')[shift_features].shift(1)
         features_df = features_df.loc[target_df.index, :] # features_dfのインデックスをtarget_dfにそろえる
         # trainとtestに分割する
         self.target_train_df, self.target_test_df = self._split_train_test(target_df, train_start_day, train_end_day, test_start_day, test_end_day)
