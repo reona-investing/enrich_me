c5c7c352b2769e84f8aa4f07c780cc6729ed00c2
reona.investing <reona.investing@gmail.com>
2024-12-28 06:52:46 +0900

    docstringの適正化

--- project/modules/jquants_api_operations/__init__.py
+++ project/modules/jquants_api_operations/__init__.py
@@ -1,5 +1,6 @@
 from .updater import update_list, update_fin, update_price
 from .processor import process_list, process_fin, process_price
+from .reader import read_list, read_fin, read_price
 
 __all__ = [
     'update_list',
@@ -7,5 +8,8 @@ __all__ = [
     'update_price',
     'process_list',
     'process_fin',
-    'process_price'
+    'process_price',
+    'read_list',
+    'read_fin',
+    'read_price',
 ]
