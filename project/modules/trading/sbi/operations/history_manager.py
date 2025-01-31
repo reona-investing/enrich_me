797078bccd8860f5b6dd6b98f5040974e7eca12e
reona-investing <reona.investing@gmail.com>
2025-02-01 00:46:10 +0900

    取引履歴取得の処理を改善

--- project/modules/trading/sbi/operations/history_manager.py
+++ project/modules/trading/sbi/operations/history_manager.py
@@ -31,41 +31,71 @@ class HistoryManager:
         self.today_margin_trades_df: 取引履歴データ
         """
         await self.page_navigator.domestic_margin()
-        html_content = await self.browser_utils.get_html_content()
-        html = soup(html_content, 'html.parser')
-        table = html.find("td", string=re.compile("銘柄"))
-        if table is None:
-            print('本日約定の注文はありません。')
-            return
-        table = table.findParent("table")
 
-        data = []
-        for tr in table.find("tbody").findAll("tr"):
-            if tr.find("td").find("a"):
-                data = self._append_contract_to_list(tr, data)
+        table_elements = []
+        
+        while True:
+            html_content = await self.browser_utils.get_html_content()
+            html = soup(html_content, 'html.parser')
+            table = html.find("td", string=re.compile("銘柄"))
+            if table is None:
+                print('本日約定の注文はありません。')
+                return
+            table = table.findParent("table")
+            for i, tr in enumerate(table.find("tbody").findAll("tr")):
+                if i > 0:
+                    table_elements.append(tr)
+            try:
+                await self.browser_utils.wait_and_click('次へ→', timeout=3)
+            except:
+                break
 
-        columns = ["日付", "売or買", "銘柄コード", "社名", "株数", "取得単価", "決済単価"]
-        self.today_margin_trades_df = pd.DataFrame(data, columns=columns)
-        self.today_margin_trades_df = self.today_margin_trades_df[(self.today_margin_trades_df['売or買']=='買')|(self.today_margin_trades_df['売or買']=='売')]
+        table_elements = self._add_previous_mtext(table_elements)   
+        df = self._get_conract_summary_df(table_elements)
+
+        df_take = df[df['売or買'].isin(['信新売', '信新買'])].rename(columns={'単価':'取得単価'})
+        df_settle = df[df['売or買'].isin(['信返売', '信返買'])].rename(columns={'単価':'決済単価'})
+
+        df = pd.merge(df_take, df_settle[['銘柄コード', '決済単価']], how='outer', on='銘柄コード').fillna(0)
+        df['売or買'] = df['売or買'].replace({'信新売':'売', '信新買':'買'})
+        # データフレームを表示
+        print(df)
+
+        self.today_margin_trades_df = df
         self.today_margin_trades_df = self._format_contracts_df(self.today_margin_trades_df, sector_list_df)
 
-    def _append_contract_to_list(self, tr:object, data:list) -> list:
-        row = []
-        text = unicodedata.normalize("NFKC", tr.findAll("td")[2].getText().strip())
-        row.append(datetime(2000+int(text[0:2]), int(text[3:5]), int(text[6:8])))
-        text = unicodedata.normalize("NFKC", tr.findAll("td")[1].getText().strip())[:3]
-        if text == '信新売':
-            row.append('売')
-        elif text == '信新買':
-            row.append('買')
-        text = unicodedata.normalize("NFKC", tr.findAll("td")[0].getText().strip())
-        row.append(text[-4:])
-        row.append(text[:-4])
-        row.append(tr.findAll("td")[3].getText().replace(",", "").strip())
-        row.append(tr.findAll("td")[4].getText().replace(",", "").strip())
-        row.append(tr.findNext("tr").findAll("td")[3].getText().replace(",", "").strip())
-        data.append(row)
-        return data
+    def _add_previous_mtext(self, html_list: list) -> list:
+        previous_mtext = None
+        for html in html_list:
+            mtext_td = html.find('td', class_='mtext')
+            if mtext_td:
+                previous_mtext = mtext_td
+            else:
+                previous_mtext_copy = soup(str(previous_mtext), 'html.parser')
+                html.insert(0, previous_mtext_copy)
+        return html_list
+
+    def _get_conract_summary_df(self, table_elements: list) -> pd.DataFrame:
+        data = []
+        for row in table_elements:
+            mtext = row.find('td', class_='mtext')
+            mbody = row.find_all('td', class_='mbody')
+
+            company_name = mtext.contents[0].get_text()
+            stock_code = mtext.contents[2].get_text()
+            type_ = mbody[0].get_text(strip=True)[:3]
+            date_text = mbody[1].get_text(strip=True).split('<br/>')[0]
+            date = datetime(2000 + int(date_text[0:2]), int(date_text[3:5]), int(date_text[6:8]))
+            shares = int(mbody[2].get_text(strip=True).replace(",", ""))
+            price = float(mbody[3].get_text(strip=True).replace(",", ""))
+
+            data.append([date, type_, stock_code, company_name, shares, price])
+        df = pd.DataFrame(data, columns=["日付", "売or買", "銘柄コード", "社名", "株数", "単価"])
+        df['単価'] *=  df['株数']
+        df = df.groupby(['日付', '売or買', '銘柄コード', '社名'])[['株数', '単価']].sum().reset_index(drop=False)
+        df['単価'] /= df['株数']
+        return df
+
 
     async def fetch_past_margin_trades(self, sector_list_df:pd.DataFrame=None, mydate:datetime=datetime.today()):
         """
@@ -179,48 +209,6 @@ class HistoryManager:
 
         return df
     
-    async def fetch_today_margin_trades(self, sector_list_df:pd.DataFrame=None):
-        """
-        直近1週間の入出金履歴をスクレイピングして取得
-        self.today_margin_trades_df: 取引履歴データ
-        """
-        await self.page_navigator.domestic_margin()
-        html_content = await self.browser_utils.get_html_content()
-        html = soup(html_content, 'html.parser')
-        table = html.find("td", string=re.compile("銘柄"))
-        if table is None:
-            print('本日約定の注文はありません。')
-            return
-        table = table.findParent("table")
-
-        data = []
-        for tr in table.find("tbody").findAll("tr"):
-            if tr.find("td").find("a"):
-                data = self._append_contract_to_list(tr, data)
-
-        columns = ["日付", "売or買", "銘柄コード", "社名", "株数", "取得単価", "決済単価"]
-        df = pd.DataFrame(data, columns=columns)
-        df = df[(df['売or買']=='買')|(df['売or買']=='売')]
-        self.today_margin_trades_df = self._format_contracts_df(df, sector_list_df)
-
-    def _append_contract_to_list(self, tr:object, data:list) -> list:
-        row = []
-        text = unicodedata.normalize("NFKC", tr.findAll("td")[2].getText().strip())
-        row.append(datetime(2000+int(text[0:2]), int(text[3:5]), int(text[6:8])))
-        text = unicodedata.normalize("NFKC", tr.findAll("td")[1].getText().strip())[:3]
-        if text == '信新売':
-            row.append('売')
-        elif text == '信新買':
-            row.append('買')
-        text = unicodedata.normalize("NFKC", tr.findAll("td")[0].getText().strip())
-        row.append(text[-4:])
-        row.append(text[:-4])
-        row.append(tr.findAll("td")[3].getText().replace(",", "").strip())
-        row.append(tr.findAll("td")[4].getText().replace(",", "").strip())
-        row.append(tr.findNext("tr").findAll("td")[3].getText().replace(",", "").strip())
-        data.append(row)
-
-        return data
     
     async def fetch_today_stock_trades(self):
         """
