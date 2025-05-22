b7c643431a05f81af56372d8ced2ba7da2bb2bca
reona-investing <reona.investing@gmail.com>
2025-05-22 12:11:23 +0900

    __init__.pyの適正化

--- project/modules/trading/sbi/__init__.py
+++ project/modules/trading/sbi/__init__.py
@@ -1,11 +1,10 @@
 
-from .operations import MarginManager, TradePossibilityManager, HistoryManager, PositionManager, TradeParameters
+from .operations import TradePossibilityManager, HistoryManager, PositionManager, TradeParameters
 from .browser import SBIBrowserManager, FileUtils
 
 __all__ = [
     "SBIBrowserManager",
     "FileUtils",
-    "MarginManager",
     "TradePossibilityManager",
     "HistoryManager",
     "PositionManager",
