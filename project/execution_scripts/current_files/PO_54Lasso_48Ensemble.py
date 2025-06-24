2ccf1724189f93df852be23693f647e8b9c7856c
reona-investing <reona.investing@gmail.com>
2025-06-24 21:49:57 +0900

    実行ファイルの更新

--- project/execution_scripts/current_files/PO_54Lasso_48Ensemble.py
+++ project/execution_scripts/current_files/PO_54Lasso_48Ensemble.py
@@ -23,23 +23,27 @@ async def main() -> None:
 
     # パラメータ設定
     # 54業種LASSO用設定
-    datasets_1st_model = f"{Paths.ML_DATASETS_FOLDER}/54sectors_LASSO_learned_in_250623"
-    sector_csv_1st_model = f"{Paths.SECTOR_REDEFINITIONS_FOLDER}/54sectors_2024-2025.csv"
-    trading_sector_num_1st_model = 2
-    candidate_sector_num_1st_model = 4
-    top_slope_1st_model = 1
+    datasets_54_lasso = f"{Paths.ML_DATASETS_FOLDER}/54sectors_LASSO_learned_in_250623"
+    sector_csv_54_lasso = f"{Paths.SECTOR_REDEFINITIONS_FOLDER}/54sectors_2024-2025.csv"
+    trading_sector_num_54_lasso = 2
+    candidate_sector_num_54_lasso = 4
+    top_slope_54_lasso = 1
 
     # 48業種LASSO+LightGBMアンサンブル用設定
-    sector_csv_2nd_model = f"{Paths.SECTOR_REDEFINITIONS_FOLDER}/48sectors_2024-2025.csv"
+    sector_csv_48_ensemble = f"{Paths.SECTOR_REDEFINITIONS_FOLDER}/48sectors_2024-2025.csv"
     sector_index_parquet = f"{Paths.SECTOR_PRICE_FOLDER}/New48sectors_price.parquet"
     datasets_path1 = f"{Paths.ML_DATASETS_FOLDER}/48sectors_LASSO_learned_in_250615"
     datasets_path2 = f"{Paths.ML_DATASETS_FOLDER}/48sectors_LightGBMlearned_in_250615"
     ensembled_datasets_path = f"{Paths.ML_DATASETS_FOLDER}/48sectors_Ensembled_learned_in_250615"
     model1_weight = 6.7
     model2_weight = 1.3
-    trading_sector_num_2nd_model = 2
-    candidate_sector_num_2nd_model = 4
-    top_slope_2nd_model = 1
+    trading_sector_num_48_ensemble = 2
+    candidate_sector_num_48_ensemble = 4
+    top_slope_48_ensemble = 1
+
+    # 各モデルへの資金配分
+    fund_allocation_54_lasso = 0.67
+    fund_allocation_48_ensemble = 0.33
 
     universe_filter = "(Listing==1)&((ScaleCategory=='TOPIX Core30')|(ScaleCategory=='TOPIX Large70')|(ScaleCategory=='TOPIX Mid400')|(ScaleCategory=='TOPIX Small 1'))"
     train_start_day = datetime(2014, 1, 1)
@@ -58,15 +62,15 @@ async def main() -> None:
         stock_dict = await data_facade.execute()
 
         # 2. 機械学習
-        ml_1st_model = LassoLearningFacade(
+        ml_54_lasso = LassoLearningFacade(
             mode=modes.machine_learning_mode,
-            dataset_path=datasets_1st_model,
+            dataset_path=datasets_54_lasso,
         ).execute()
 
-        ml_2nd_model = MachineLearningFacade(
+        ml_48_ensemble = MachineLearningFacade(
             mode=modes.machine_learning_mode,
             stock_dfs_dict=stock_dict,
-            sector_redef_csv_path=sector_csv_2nd_model,
+            sector_redef_csv_path=sector_csv_48_ensemble,
             sector_index_parquet_path=sector_index_parquet,
             datasets1_path=datasets_path1,
             datasets2_path=datasets_path2,
@@ -83,20 +87,20 @@ async def main() -> None:
         trade_facade = TradingFacade()
         configs = [
             ModelOrderConfig(
-                ml_datasets=ml_1st_model,
-                sector_csv=sector_csv_1st_model,
-                trading_sector_num=trading_sector_num_1st_model,
-                candidate_sector_num=candidate_sector_num_1st_model,
-                top_slope=top_slope_1st_model,
-                margin_weight=0.67,
+                ml_datasets=ml_54_lasso,
+                sector_csv=sector_csv_54_lasso,
+                trading_sector_num=trading_sector_num_54_lasso,
+                candidate_sector_num=candidate_sector_num_54_lasso,
+                top_slope=top_slope_54_lasso,
+                margin_weight=fund_allocation_54_lasso,
             ),
             ModelOrderConfig(
-                ml_datasets=ml_2nd_model,
-                sector_csv=sector_csv_2nd_model,
-                trading_sector_num=trading_sector_num_2nd_model,
-                candidate_sector_num=candidate_sector_num_2nd_model,
-                top_slope=top_slope_2nd_model,
-                margin_weight=0.33,
+                ml_datasets=ml_48_ensemble,
+                sector_csv=sector_csv_48_ensemble,
+                trading_sector_num=trading_sector_num_48_ensemble,
+                candidate_sector_num=candidate_sector_num_48_ensemble,
+                top_slope=top_slope_48_ensemble,
+                margin_weight=fund_allocation_48_ensemble,
             ),
         ]
 
