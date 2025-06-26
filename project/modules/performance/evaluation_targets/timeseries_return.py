d09f2d662d6d7d60c625eb69d08ee6ef0cae0f2c
reona-investing <reona.investing@gmail.com>
2025-06-27 00:50:28 +0900

    Move timeseries_return to evaluation_targets

--- project/modules/performance/evaluation_targets/timeseries_return.py
+++ project/modules/performance/evaluation_targets/timeseries_return.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import pandas as pd
 
-from .transformation import TaxRate, Leverage
+from ..transformation import TaxRate, Leverage
 from dataclasses import dataclass
 
 
