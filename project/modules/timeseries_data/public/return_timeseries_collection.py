3bfe2146696962131f666897610f6eb21df4eca7
reona-investing <reona.investing@gmail.com>
2025-07-12 00:56:57 +0900

    ReturnTimeseriesCollectionの修正

--- project/modules/timeseries_data/public/return_timeseries_collection.py
+++ project/modules/timeseries_data/public/return_timeseries_collection.py
@@ -4,24 +4,50 @@ from timeseries_data.public.return_timeseries import ReturnTimeseries
 
 
 class ReturnTimeseriesCollection:
+    """複数のReturnTimeseriesインスタンスを格納するクラス"""
 
     def __init__(self):
         self._collection: list[ReturnTimeseries] = []
 
     def append(self, return_timeseries: ReturnTimeseries):
+        """
+        ReturnTimeseriesインスタンスを追加します。
+        
+        Args:
+            return_timeseries (ReturnTimeseries): 追加するインスタンス
+        """
         if return_timeseries.name in self.names:
             raise ValueError(f"{return_timeseries.name}インスタンスはすでに登録済みです。登録されるインスタンスの名称は一意である必要があります。")
         self._collection.append(return_timeseries)
 
     def clear(self):
+        """
+        格納したReturnTimeseriesインスタンスをすべて削除します。
+        """
         self._collection = []
 
     def get_return_timeseries_instance(self, instance_name: str) -> ReturnTimeseries:
+        """
+        指定した名前のインスタンスを取り出します。
+       
+        Args:
+            instance_name (str): 取り出したいインスタンスの名前
+        """
+        if self.length == 0:
+            raise ValueError(f"まだインスタンスが1つも登録されていません。")
         instance_list = [instance for instance in self._collection if instance.name == instance_name]
         if len(instance_list):
             raise ValueError(f"{instance_name}インスタンスは存在しません。")
         return instance_list[0]
 
+    def get_all_return_timeseries_instances(self) -> list[ReturnTimeseries]:
+        """
+        格納しているすべてのReturnTimeseriesインスタンスをリスト形式で取得します。
+        """
+        if self.length == 0:
+            return []
+        return [instance for instance in self._collection]
+    
     @property
     def length(self) -> int:
         """格納しているReturnTimeseriesインスタンスの個数を返します。"""
