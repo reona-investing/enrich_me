29b1c162d47c0ee537ea3ac6d879420acd65a8ef
reona-investing <reona.investing@gmail.com>
2025-07-11 21:41:25 +0900

    Rename BondDailyReturn class and file

--- project/modules/timeseries_data/calculation_method/__init__.py
+++ project/modules/timeseries_data/calculation_method/__init__.py
@@ -2,3 +2,4 @@ from .base import CalculationMethodBase
 from .intraday_return import IntradayReturn
 from .overnight_return import OvernightReturn
 from .daily_return import DailyReturn
+from .bond_daily_return import BondDailyReturn
